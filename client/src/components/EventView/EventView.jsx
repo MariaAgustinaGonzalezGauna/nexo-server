127627372f8d607377c975e7b90fba4c05c9ef2f
Agustin Martinez <agustinfuentesmartinez05@gmail.com>
2025-06-04 23:27:28 -0300

    se modifico el css

--- client/src/components/EventView/EventView.jsx
+++ client/src/components/EventView/EventView.jsx
@@ -5,18 +5,37 @@ import { useLocation } from "react-router"
 const EventView = () =>
 {
   const localization=useLocation()  
-  const {image,title,date,location} = localization.state || {}
+  const {image,title,date,location,descripcion} = localization.state || {}
   return (
     <div>
       <div className="event-info-container">
         <div className="imagenes">
-         <img src={image} alt="" width={"300px"} height={"300px"} />
+         <img src={image} alt="" width={"400px"} height={"400px"} />
          </div>
          <div className="info">
          <h2>{title}</h2>
          <h3>{location}</h3>
          <p>{date}</p>
+         <div className="exp">
+         <button>Compartir</button>
+          <div className="puntuacion">
+            <p>☆</p>
+            <p>☆</p>
+            <p>☆</p>
+            <p>☆</p>
+            <p>☆</p>
+          </div>
+          </div>
          </div>
+      </div>
+      <div className="mapa-desc">
+        <div className="maps">
+          *
+        </div>
+        <div className="desc">
+         <h2>Descripción</h2> 
+        <p>{descripcion}</p>
+        </div>
       </div>
       
     </div>
