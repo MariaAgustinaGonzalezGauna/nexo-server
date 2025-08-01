15a39c84f8d20ddaf9c849fb913f1c966722c2e7
Agustin <agustinfuentesmartinez05@gmail.com>
2025-08-01 15:16:57 -0300

    cambio de url

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Configuración de CORS
 app.use(cors({
-  origin: "https://nexo-server-hgg1.vercel.app", // URL del frontend
+  origin: "https://nexo-server-kuzz.vercel.app", // URL del frontend
   credentials: true // Permitir cookies y headers de autenticación
 }));
 
