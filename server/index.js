6985df4b93da455671db8734d26273ee37cc24dd
Agustin <agustinfuentesmartinez05@gmail.com>
2025-08-03 17:02:00 -0300

    correcciones terminadas

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Configuración de CORS
 app.use(cors({
-  origin: "https://nexo-server-uenc.vercel.app", // URL del frontend
+  origin: "https://nexo-server-wi34.vercel.app", // URL del frontend
   credentials: true // Permitir cookies y headers de autenticación
 }));
 
